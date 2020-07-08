9cf630dd8b81b172869e52d38f43d113a049edec
Matthew <matthew25kim@gmail.com>
2020-07-07 23:01:35 -0400

    remove selenium stuff

--- service/service.go
+++ service/service.go
@@ -5,12 +5,7 @@ import (
 	"gobuy/uniqlo"
 	"net/http"
 	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 
-	log "github.com/sirupsen/logrus"
-	"github.com/tebeka/selenium"
 	"github.com/zpatrick/go-config"
 )
 
@@ -30,34 +25,34 @@ func Start() {
 		fmt.Println("server up")
 	}()
 
-	caps := selenium.Capabilities{"browserName": "chrome"}
-	wd, err := selenium.NewRemote(caps, "https://pure-headland-22862.herokuapp.com/")
-	if err != nil {
-		log.Error(err.Error())
-	}
+	// caps := selenium.Capabilities{"browserName": "chrome"}
+	// wd, err := selenium.NewRemote(caps, "https://pure-headland-22862.herokuapp.com/")
+	// if err != nil {
+	// 	log.Error(err.Error())
+	// }
 
-	defer wd.Quit()
+	// defer wd.Quit()
 
-	if s.uniqlo, err = uniqlo.TestSpawn(wd, config); err != nil {
-		log.Error("couldn't start uniqlo service")
-	}
+	// if s.uniqlo, err = uniqlo.TestSpawn(wd, config); err != nil {
+	// 	log.Error("couldn't start uniqlo service")
+	// }
 
-	// block until OS signal received
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	// // block until OS signal received
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	// var wg sync.WaitGroup
+	// wg.Add(1)
 
-	go func() {
-		defer wg.Done()
-		log.Info("waiting for signal")
-		receivedSignal := <-c
-		log.Info(fmt.Sprintf("received signal: %s", receivedSignal))
-		s.Stop()
-	}()
+	// go func() {
+	// 	defer wg.Done()
+	// 	log.Info("waiting for signal")
+	// 	receivedSignal := <-c
+	// 	log.Info(fmt.Sprintf("received signal: %s", receivedSignal))
+	// 	s.Stop()
+	// }()
 
-	wg.Wait()
+	// wg.Wait()
 }
 
 func (s *T) Stop() {
